15c1bb19eddd0fca7dd707ded73a8d2cb5f4ef26
a1t8t8u9-ops <a1t8t8u9@gmail.com>
2026-08-30 22:10:38 +0800

    Bump PWA cache to v0.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="scar-scanner-v04";
+const CACHE="scar-scanner-v05";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
